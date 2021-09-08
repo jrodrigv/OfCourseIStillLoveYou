6cfbb75689352555e92018b83d42cf87a15378e9
Jesus Rodriguez Valencia <jesus.rodriguez.valencia@gmail.com>
2021-09-08 21:00:35 +0200

    Adding assembly information

--- OfCourseIStillLoveYou/Properties/AssemblyInfo.cs
+++ OfCourseIStillLoveYou/Properties/AssemblyInfo.cs
@@ -2,37 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OfCourseIStillLoveYou.TUFX")]
+[assembly: AssemblyTitle("OfCourseIStillLoveYou")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OfCourseIStillLoveYou.TUFX")]
+[assembly: AssemblyCompany("jrodrigv")]
+[assembly: AssemblyProduct("OfCourseIStillLoveYou.Properties")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d77c6684-5d8a-4e3c-bfe0-c2b8f54dbaab")]
+[assembly: Guid("7b4f15e2-dbfa-4412-a62b-3b543ea8a204")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: KSPAssembly("OfCourseIStillLoveYou.TUFX", 1, 0)]
+[assembly: KSPAssembly("OfCourseIStillLoveYou", 1, 0)]
